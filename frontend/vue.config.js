a9ea88f5db414681b8aaa3410580f325d21bcec2
touka.hanetsuki <touka.hanetuki@gmail.com>
2020-12-31 22:32:10 +0900

    create volumes

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,15 +1,12 @@
 module.exports = {
-  // proxy API requests to Valet during development
   devServer: {
-    proxy: 'http://laracon.test'
+    proxy: {
+      '^/api': {
+        target: 'http://nginx'
+      }
+    }
   },
-
-  // output built static files to Laravel's public dir.
-  // note the "build" script in package.json needs to be modified as well.
-  outputDir: '../backend/public',
-
-  // modify the location of the generated HTML file.
-  // make sure to do this only in production.
+  outputDir: '../public',
   indexPath: process.env.NODE_ENV === 'production'
     ? '../resources/views/index.blade.php'
     : 'index.html'
